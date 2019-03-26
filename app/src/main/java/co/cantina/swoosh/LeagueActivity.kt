b99f1fa2df87902fa12ec1b2d132a80904644e5d
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-26 16:21:27 -0400

    added button functions

--- app/src/main/java/co/cantina/swoosh/LeagueActivity.kt
+++ app/src/main/java/co/cantina/swoosh/LeagueActivity.kt
@@ -3,9 +3,12 @@ package co.cantina.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,27 @@ class LeagueActivity : BaseActivity() {
     fun leagueNextClicked(view: View){
         val leagueIntent = Intent(this, SkillActivity::class.java)
         startActivity(leagueIntent)
+    }
+
+    fun onMensClicked(view: View){
+        womensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "Mens"
+    }
+
+    fun onWomensClicked(view: View){
+        mensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "Womens"
+    }
+
+    fun onCoedClicked(view: View){
+        womensLeagueButton.isChecked = false
+        mensLeagueButton.isChecked = false
+
+        selectedLeague = "Co-Ed"
+
     }
 }
