c830b2669a75bf6eb16c864c017441fb308f6df1
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-26 15:57:57 -0400

    added onClick to leagueNextButton to transition to skill activity

--- app/src/main/java/co/cantina/swoosh/LeagueActivity.kt
+++ app/src/main/java/co/cantina/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package co.cantina.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        leagueNextButton.setOnClickListener {
+            val leagueIntent = Intent(this, SkillActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
