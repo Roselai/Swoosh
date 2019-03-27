8ffed654d12272073c76d772ba29be7117e84c34
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-27 08:27:11 -0400

    fixed missing import

---
+++ app/src/main/java/co/cantina/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,13 @@
+package co.cantina.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.cantina.swoosh.R
+
+class FinishActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+    }
+}
